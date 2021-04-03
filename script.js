ad4307fcabea7341c184e9bb0e10fef2b47066b4
plskz <zaisantillan14@gmail.com>
2021-04-03 18:51:52 +0800

    draft

---
+++ script.js
@@ -0,0 +1,51 @@
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        return "draw";
+    }
+
+    if (playerSelection == "rock") {
+        if (computerSelection == "paper") {
+            return "computer win";
+        } else {
+            return "you win";
+        }
+    }
+
+    if (playerSelection == "paper") {
+        if (computerSelection == "scissors") {
+            return "computer win";
+        } else {
+            return "you win";
+        }
+    }
+
+    if (playerSelection == "scissors") {
+        if (computerSelection == "rock") {
+            return "computer win";
+        } else {
+            return "you win";
+        }
+    }
+}
+
+let computer = () => {
+    let picks = ["rock", "paper", "scissors"];
+    return picks[Math.floor(Math.random() * 3)];
+};
+
+function game() {
+    let scoreUser = 0
+    let scoreComputer = 0;
+
+    for(let i=0; i<5; i++) {
+        let player = prompt("rock-paper-scissors");
+        let comp = computer();
+        console.log(`${player} vs ${comp}`);
+        console.log(playRound(player, comp));
+
+    }
+}
+
+game();
