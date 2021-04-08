498f4707d67fc7475df6b13a30b1f4a09b9fc03f
plskz <zaisantillan14@gmail.com>
2021-04-08 21:56:44 +0800

    Update

--- script.js
+++ script.js
@@ -1,51 +1,58 @@
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
+let playerScore = 0;
+let computerScore = 0;
 
-    if (playerSelection === computerSelection) {
-        return "draw";
-    }
+const paraPlayerScore = document.querySelector(".player-score");
+const paraComputerScore = document.querySelector(".computer-score");
+const heading = document.querySelector(".score-heading");
 
-    if (playerSelection == "rock") {
-        if (computerSelection == "paper") {
-            return "computer win";
-        } else {
-            return "you win";
-        }
-    }
+const hiddenEl = document.querySelector('.hidden');
+const btnReset = document.querySelector('.btn-reset');
 
-    if (playerSelection == "paper") {
-        if (computerSelection == "scissors") {
-            return "computer win";
-        } else {
-            return "you win";
-        }
-    }
+const buttons = document.querySelectorAll(".btn");
+buttons.forEach((btn) => btn.addEventListener("click", playRound));
 
-    if (playerSelection == "scissors") {
-        if (computerSelection == "rock") {
-            return "computer win";
-        } else {
-            return "you win";
-        }
-    }
-}
-
-let computer = () => {
+function computer() {
     let picks = ["rock", "paper", "scissors"];
     return picks[Math.floor(Math.random() * 3)];
-};
-
-function game() {
-    let scoreUser = 0
-    let scoreComputer = 0;
-
-    for(let i=0; i<5; i++) {
-        let player = prompt("rock-paper-scissors");
-        let comp = computer();
-        console.log(`${player} vs ${comp}`);
-        console.log(playRound(player, comp));
-
-    }
 }
 
-game();
+function playRound(e) {
+    playerSelection = e.target.name;
+    computerSelection = computer();
+
+    console.log(`player: ${playerSelection}`);
+    console.log(`computer: ${computerSelection}`);
+
+    if (playerScore == 5 || computerScore == 5) {
+        hiddenEl.classList.remove('hidden')
+        return;
+    }
+
+    if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+        playerScore++;
+        heading.textContent = "You won!";
+    } else if (playerSelection === computerSelection) {
+        heading.textContent = "Draw!";
+    } else {
+        computerScore++;
+        heading.textContent = "You lost!";
+    }
+
+    // update the score
+    paraPlayerScore.textContent = `Player: ${playerScore}`;
+    paraComputerScore.textContent = `Computer: ${computerScore}`;
+}
+
+// reset 
+btnReset.addEventListener('click', () => {
+    heading.textContent = "Score";
+    hiddenEl.classList.add('hidden');
+    playerScore = 0;
+    computerScore = 0;
+    paraPlayerScore.textContent = `Player: ${playerScore}`;
+    paraComputerScore.textContent = `Computer: ${computerScore}`;
+})
