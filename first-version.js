460444aa605d0d5504bc52d641a215a2b114a9ff
plskz <zaisantillan14@gmail.com>
2021-06-10 22:55:07 +0800

    :art:

--- first-version.js
+++ first-version.js
@@ -1,52 +1,50 @@
 function computer() {
-    let picks = ["rock", "paper", "scissors"];
-    return picks[Math.floor(Math.random() * 3)];
+  let picks = ["rock", "paper", "scissors"];
+  return picks[Math.floor(Math.random() * 3)];
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
+  playerSelection = playerSelection.toLowerCase();
 
-    if (playerSelection === computerSelection) {
-        return "draw";
-    }
+  if (playerSelection === computerSelection) {
+    return "draw";
+  }
 
-    if (playerSelection == "rock") {
-        if (computerSelection == "paper") {
-            return "computer win";
-        } else {
-            return "you win";
-        }
+  if (playerSelection == "rock") {
+    if (computerSelection == "paper") {
+      return "computer win";
+    } else {
+      return "you win";
     }
+  }
 
-    if (playerSelection == "paper") {
-        if (computerSelection == "scissors") {
-            return "computer win";
-        } else {
-            return "you win";
-        }
+  if (playerSelection == "paper") {
+    if (computerSelection == "scissors") {
+      return "computer win";
+    } else {
+      return "you win";
     }
+  }
 
-    if (playerSelection == "scissors") {
-        if (computerSelection == "rock") {
-            return "computer win";
-        } else {
-            return "you win";
-        }
+  if (playerSelection == "scissors") {
+    if (computerSelection == "rock") {
+      return "computer win";
+    } else {
+      return "you win";
     }
+  }
 }
 
-
 function game() {
-    let scoreUser = 0
-    let scoreComputer = 0;
+  let scoreUser = 0;
+  let scoreComputer = 0;
 
-    for(let i=0; i<5; i++) {
-        let player = prompt("rock-paper-scissors");
-        let comp = computer();
-        console.log(`${player} vs ${comp}`);
-        console.log(playRound(player, comp));
-
-    }
+  for (let i = 0; i < 5; i++) {
+    let player = prompt("rock-paper-scissors");
+    let comp = computer();
+    console.log(`${player} vs ${comp}`);
+    console.log(playRound(player, comp));
+  }
 }
 
 game();
--- script.js
+++ script.js
@@ -5,54 +5,54 @@ const paraPlayerScore = document.querySelector(".player-score");
 const paraComputerScore = document.querySelector(".computer-score");
 const heading = document.querySelector(".score-heading");
 
-const hiddenEl = document.querySelector('.hidden');
-const btnReset = document.querySelector('.btn-reset');
+const hiddenEl = document.querySelector(".hidden");
+const btnReset = document.querySelector(".btn-reset");
 
 const buttons = document.querySelectorAll(".btn");
 buttons.forEach((btn) => btn.addEventListener("click", playRound));
 
 function computer() {
-    let picks = ["rock", "paper", "scissors"];
-    return picks[Math.floor(Math.random() * 3)];
+  let picks = ["rock", "paper", "scissors"];
+  return picks[Math.floor(Math.random() * 3)];
 }
 
 function playRound(e) {
-    playerSelection = e.target.name;
-    computerSelection = computer();
+  playerSelection = e.target.name;
+  computerSelection = computer();
 
-    console.log(`player: ${playerSelection}`);
-    console.log(`computer: ${computerSelection}`);
+  console.log(`player: ${playerSelection}`);
+  console.log(`computer: ${computerSelection}`);
 
-    if (playerScore == 5 || computerScore == 5) {
-        hiddenEl.classList.remove('hidden')
-        return;
-    }
+  if (playerScore == 5 || computerScore == 5) {
+    hiddenEl.classList.remove("hidden");
+    return;
+  }
 
-    if (
-        (playerSelection === "rock" && computerSelection === "scissors") ||
-        (playerSelection === "paper" && computerSelection === "rock") ||
-        (playerSelection === "scissors" && computerSelection === "paper")
-    ) {
-        playerScore++;
-        heading.textContent = "You won!";
-    } else if (playerSelection === computerSelection) {
-        heading.textContent = "Draw!";
-    } else {
-        computerScore++;
-        heading.textContent = "You lost!";
-    }
+  if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    playerScore++;
+    heading.textContent = "You won!";
+  } else if (playerSelection === computerSelection) {
+    heading.textContent = "Draw!";
+  } else {
+    computerScore++;
+    heading.textContent = "You lost!";
+  }
 
-    // update the score
-    paraPlayerScore.textContent = `Player: ${playerScore}`;
-    paraComputerScore.textContent = `Computer: ${computerScore}`;
+  // update the score
+  paraPlayerScore.textContent = `Player: ${playerScore}`;
+  paraComputerScore.textContent = `Computer: ${computerScore}`;
 }
 
-// reset 
-btnReset.addEventListener('click', () => {
-    heading.textContent = "Score";
-    hiddenEl.classList.add('hidden');
-    playerScore = 0;
-    computerScore = 0;
-    paraPlayerScore.textContent = `Player: ${playerScore}`;
-    paraComputerScore.textContent = `Computer: ${computerScore}`;
-})
+// reset
+btnReset.addEventListener("click", () => {
+  heading.textContent = "Score";
+  hiddenEl.classList.add("hidden");
+  playerScore = 0;
+  computerScore = 0;
+  paraPlayerScore.textContent = `Player: ${playerScore}`;
+  paraComputerScore.textContent = `Computer: ${computerScore}`;
+});
